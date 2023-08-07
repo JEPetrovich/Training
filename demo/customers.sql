b6ea01c7c8183752cae0fedd9ba4c1e72efdebc8
JEPetrovich <epetrovich@confluencia.fto.com.ar>
2023-08-07 08:16:27 -0300

    Add customers script sql

--- demo/customers.sql
+++ demo/customers.sql
@@ -1,13 +1,27 @@
 /*
--- Query: SELECT id, mail, name, lastname, dateOfBirth FROM demo.customer
--- Date: 2023-08-06 17:51
+-- Query: SELECT * FROM demo.customer
+-- Date: 2023-08-07 08:14
 */
-INSERT INTO `` (`id`,`mail`,`name`,`lastname`,`dateOfBirth`) VALUES (14,'spidey@marvel.com','Peter','Parker','1999-04-05 00:00:00.000000');
-INSERT INTO `` (`id`,`mail`,`name`,`lastname`,`dateOfBirth`) VALUES (17,'hulk@marvel.com','Bruce','Banner','1990-06-17 00:00:00.000000');
-INSERT INTO `` (`id`,`mail`,`name`,`lastname`,`dateOfBirth`) VALUES (19,'blackwidow@marvel.com','Natasha','Romanof','1990-12-07 00:00:00.000000');
-INSERT INTO `` (`id`,`mail`,`name`,`lastname`,`dateOfBirth`) VALUES (20,'ironman@marvel.com','Tony','Stark','1980-07-14 00:00:00.000000');
-INSERT INTO `` (`id`,`mail`,`name`,`lastname`,`dateOfBirth`) VALUES (21,'venom@marvel.com','Eddie','Brock','1990-05-14 00:00:00.000000');
-INSERT INTO `` (`id`,`mail`,`name`,`lastname`,`dateOfBirth`) VALUES (22,'captainamerica@marvel.com','Steve','Rogers','1980-09-18 00:00:00.000000');
-INSERT INTO `` (`id`,`mail`,`name`,`lastname`,`dateOfBirth`) VALUES (23,'absorbingman@marvel.com','Rocky','Davis','1980-05-15 00:00:00.000000');
-INSERT INTO `` (`id`,`mail`,`name`,`lastname`,`dateOfBirth`) VALUES (24,'wolverine@marvel.com','James ','Howlett','1930-05-20 00:00:00.000000');
-INSERT INTO `` (`id`,`mail`,`name`,`lastname`,`dateOfBirth`) VALUES (25,'punisher@marvel.com','Frank ','Castle','1975-05-19 00:00:00.000000');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (14,'1999-04-05 00:00:00.000000','Parker','spidey@marvel.com','Peter');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (17,'1990-06-17 00:00:00.000000','Banner','hulk@marvel.com','Bruce');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (19,'1990-12-07 00:00:00.000000','Romanof','blackwidow@marvel.com','Natasha');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (20,'1980-07-14 00:00:00.000000','Stark','ironman@marvel.com','Tony');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (21,'1990-05-14 00:00:00.000000','Brock','venom@marvel.com','Eddie');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (22,'1980-09-18 00:00:00.000000','Rogers','captainamerica@marvel.com','Steve');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (24,'1930-05-20 00:00:00.000000','Howlett','wolverine@marvel.com','James ');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (25,'1975-05-19 00:00:00.000000','Castle','punisher@marvel.com','Frank ');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (26,'1980-06-21 00:00:00.000000','Davis','absorbingman@marvel.com','Rocky');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (27,'1978-07-11 00:00:00.000000','Fury','nickfury@marvel.com','Nicholas Joseph');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (28,'1985-09-11 00:00:00.000000','Challa','blackpanther@marvel.com','T');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (29,'1986-04-18 00:00:00.000000','Murdock','daredevil@marvel.com','Matthew Michael');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (30,'1990-02-19 00:00:00.000000','Wilson','deadpool@marvel.com','Wade Winston ');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (31,'1980-09-16 00:00:00.000000','Rasputin','colossus@marvel.com','Piotr Nikolaievitch ');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (32,'1975-02-19 00:00:00.000000','Strange','drstrange@marvel.com','Stephen Vincent ');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (33,'1980-08-19 00:00:00.000000','Grimm','thing@marvel.com','Benjamin Jacob ');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (34,'1980-05-14 00:00:00.000000','McCoy','beast@marvel.com','Henry Philip ');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (35,'1980-09-17 00:00:00.000000','Pryde','kittypryde@marvel.com','Katherine Anne ');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (36,'1985-09-19 00:00:00.000000','Barton','hawkeye@marvel.com','Clinton Francis');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (37,'1978-08-12 00:00:00.000000','Lucas','lukecage@marvel.com','Carl ');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (38,'1990-08-05 00:00:00.000000','Rand-K\'ai','ironfist@marvel.com','Daniel Thomas ');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (39,'1990-01-30 00:00:00.000000','Maximoff','scarletwitch@marvel.com','Wanda Marya');
+INSERT INTO `` (`id`,`dateOfBirth`,`lastName`,`mail`,`name`) VALUES (40,'2200-09-17 00:00:00.000000','Summers','cable@marvel.com','Nathan Christopher Charles ');
